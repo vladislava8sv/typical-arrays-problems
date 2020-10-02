facb41f36cd34e428fbbe7292b4d3d91f5dc6345
vladislava8sv <57909332+vladislava8sv@users.noreply.github.com>
2020-10-02 23:17:51 +0300

    feat: add max, min and avg functions

--- src/index.js
+++ src/index.js
@@ -1,12 +1,39 @@
 
 exports.min = function min (array) {
-  return 0;
+  if (array==undefined|| array.length == 0) {
+    return 0;
+  }
+  else {
+  var arr_min = array[0];
+  for (i = 1; i < array.length; i++) {
+     if (array[i] <= arr_min) {
+        arr_min = array[i];   
+     }
+  }
+  return arr_min;
+}
 }
 
 exports.max = function max (array) {
-  return 0;
+  if (array==undefined|| array.length == 0) {
+    return 0;
+  }
+  var arr_max = array[0];
+  for (i = 1; i < array.length; i++) {
+     if (array[i] >= arr_max) {
+        arr_max = array[i];   
+     }
+  }
+  return arr_max;
 }
 
 exports.avg = function avg (array) {
-  return 0;
+  if (array==undefined|| array.length == 0) {
+    return 0;
+  }
+  var arr_avg = array[0];
+  for (i = 1; i < array.length; i++) {
+        arr_avg += array[i];   
+  }
+  return arr_avg/array.length;
 }
